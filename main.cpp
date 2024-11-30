e98393d70e40f36c205ebf3c1d1fce56b4b0058c
tongshengw <trinityrushworth@gmail.com>
2024-11-30 02:13:52 -0500

    rendering spheres working

--- main.cpp
+++ main.cpp
@@ -1,13 +1,81 @@
 #include <SFML/Graphics.hpp>
-#include "Card.hpp"
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+struct Sphere {
+    float x;
+    float y;
+    float z;
+};
+
+Sphere & sphere_init(Sphere &s, float x, float y, float z) {
+    s.x = x;
+    s.y = y;
+    s.z = z;
+    return s;
+}
+
+// s is the input vector to be projected
+sf::Vector2f project(sf::Vector3f cameraPosition, sf::Vector3f cameraRotation,
+                     sf::Vector3f s, float focalLength, int winSizeX, int winSizeY) {
+  sf::Vector3f cameraRelPos;
+
+  float X = s.x - cameraPosition.x;
+  float Y = s.y - cameraPosition.y;
+  float Z = s.z - cameraPosition.z;
+
+  cameraRelPos.x = cos(cameraRotation.y) *
+                       (sin(cameraRotation.z) * Y + cos(cameraRotation.z) * X) -
+                   sin(cameraRotation.y) * Z;
+  cameraRelPos.y = sin(cameraRotation.x) *
+                       (cos(cameraRotation.y) * Z +
+                        sin(cameraRotation.y) * (sin(cameraRotation.z) * Y +
+                                                 cos(cameraRotation.z) * X)) +
+                   cos(cameraRotation.x) *
+                       (cos(cameraRotation.z) * Y - sin(cameraRotation.z) * X);
+  cameraRelPos.z = cos(cameraRotation.x) *
+                       (cos(cameraRotation.y) * Z +
+                        sin(cameraRotation.y) * (sin(cameraRotation.z) * Y +
+                                                 cos(cameraRotation.z) * X)) -
+                   sin(cameraRotation.x) *
+                       (cos(cameraRotation.z) * Y - sin(cameraRotation.z) * X);
+
+  sf::Vector2f projected;
+
+  projected.x = (focalLength / cameraRelPos.z) * cameraRelPos.x + winSizeX / 2;
+  projected.y = (focalLength / cameraRelPos.z) * cameraRelPos.y + winSizeY / 2;
+
+  return projected;
+}
+
+sf::Clock deltaTimeClock;
+sf::Time deltaTime;
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    
+    vector<Sphere> spheres;
+    Sphere s1;
+    Sphere s2;
+    Sphere s3;
+    spheres.push_back(sphere_init(s1, 0, 0, 10));
+    // spheres.push_back(sphere_init(s2, 60, 0, 30));
+    // spheres.push_back(sphere_init(s3, 0, 60, 30));
+    
 
-    Card card1("test", 1, 10, 10);
+    sf::Vector3f cameraPosition(0, 0, 0);
+    sf::Vector3f cameraRotation(0, 0, 0);
+
+    float focalLength = 10;
+    float sphereRadius = 30;
+
+    float cameraPolarAngle = 0;
+    float cameraPolarDisplacement = 50;
 
     while (window.isOpen())
     {
@@ -18,9 +86,41 @@ int main()
                 window.close();
         }
 
+        deltaTime = deltaTimeClock.restart();
+
+        // cameraPolarAngle += deltaTime.asSeconds();
+        // cameraPosition.x = cameraPolarDisplacement * cos(cameraPolarAngle);
+        // cameraPosition.y = cameraPolarDisplacement * sin(cameraPolarAngle);
+
+        // cameraRotation.x = cameraPolarAngle;
+
         window.clear();
-        window.draw(shape);
-        card1.render(window);
+
+        for (Sphere s : spheres) {
+            sf::Vector3f sphereEdgeVector;
+            sf::Vector3f sphereVector(s.x, s.y, s.z);
+            sphereEdgeVector = sf::Vector3f(s.x + sphereRadius, s.y, s.z);
+
+            int winSizeX = window.getSize().x;
+            int winSizeY = window.getSize().y;
+
+            sf::Vector2f projectedSphereVector = project(cameraPosition, cameraRotation, sphereVector, focalLength, winSizeX, winSizeY);
+            sf::Vector2f projectedSphereEdgeVector = project(cameraPosition, cameraRotation, sphereEdgeVector, focalLength, winSizeX, winSizeY);
+
+            sf::Vector2f diff = projectedSphereEdgeVector - projectedSphereVector;
+
+            float projectedRadius = sqrt(diff.x*diff.x + diff.y*diff.y);
+
+            window.setView(sf::View(sf::FloatRect(0.f, 0.f, window.getSize().x, window.getSize().y)));
+
+            sf::CircleShape circle;
+            circle.setRadius(projectedRadius);
+            circle.setFillColor(sf::Color::Green);
+            circle.setOrigin(projectedRadius, projectedRadius);
+            circle.setPosition(projectedSphereVector.x, projectedSphereVector.y);
+            window.draw(circle);
+        }
+        
         window.display();
     }
 
--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
-g++ -c main.cpp Card.cpp -I"C:\Users\tongs\Documents\code\libraries\SFML-2.6.2-64\include" -DSFML_STATIC 
-g++ main.o Card.o -o main -L"C:\Users\tongs\Documents\code\libraries\SFML-2.6.2-64\lib" -lsfml-graphics -lsfml-window -lsfml-system -std=c++17
+g++ -c main.cpp -I"C:\Users\tongs\Documents\code\libraries\SFML-2.6.2-64\include" -DSFML_STATIC 
+g++ main.o -o main -L"C:\Users\tongs\Documents\code\libraries\SFML-2.6.2-64\lib" -lsfml-graphics -lsfml-window -lsfml-system -std=c++17
 .\main.exe
